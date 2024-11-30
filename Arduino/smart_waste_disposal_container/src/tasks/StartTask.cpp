6b28bd82fecb3700af1efa8b722cb1a9dc178c64
TabakuEraldo <152480725+TabakuEraldo@users.noreply.github.com>
2024-11-30 21:23:33 +0100

    aggiunta cartella per i task

---
+++ Arduino/smart_waste_disposal_container/src/tasks/StartTask.cpp
@@ -0,0 +1,16 @@
+#include "tasks/StartTask.h"
+#include <Arduino.h>
+
+
+StartTask::StartTask(WasteCollectorPlant* wasteCollector):
+    wasteCollector(wasteCollector){
+    setState(IDLE);
+    }
+
+void StartTask::tick(){
+    switch(state){
+        case IDLE:{
+            
+        }
+    }
+}
---
+++ Arduino/smart_waste_disposal_container/src/tasks/StartTask.h
@@ -0,0 +1,7 @@
+#ifndef __START_TASK__
+#define __START_TASK__
+
+
+//il model?
+#include "serial/Task.h"
+#include 
