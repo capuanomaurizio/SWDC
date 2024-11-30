d03e66f521f6c4cf867a7f302d697d07bf3b39e4
TabakuEraldo <152480725+TabakuEraldo@users.noreply.github.com>
2024-11-30 21:22:14 +0100

    aggiunto file di configurazione dei pin

---
+++ Arduino/smart_waste_disposal_container/src/config.h
@@ -0,0 +1,13 @@
+#define LED_GREEN 7
+#define LED_RED 8
+#define BUTTON_OPEN 3
+#define BUTTON_CLOSE 4
+#define PIR_PIN 2
+#define SERVO 9
+#define SONAR_TRIG_PIN 12
+#define SONAR_ECHO_PIN 13
+#define TEMP_PIN A0
+
+#define CLOSE_SERVO 90
+#define OPEN_SERVO 180
+#define EMPTY_SERVO 0
